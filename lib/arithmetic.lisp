7e26e7765b800172ec37a8e36b421dadb5c70804
AugustusHuang <augustushwang@gmail.com>
2015-08-15 13:57:31 +0800

    Move base arithmetic functions to CL-QUANTUM package

--- lib/arithmetic.lisp
+++ lib/arithmetic.lisp
@@ -21,27 +21,28 @@
 ;;;; ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 ;;;; OTHER DEALINGS IN THE SOFTWARE.
 
-;;;; General arithmetic functions for emulator
+(in-package :cl-quantum)
 
-(in-package :cl-quantum-emulator)
+(defun expt-mod (n a width ancilla qreg)
+  "Quantum exponent and modulus algorithm."
+  (declare (type fixnum width ancilla)
+	   (type quantum-register qreg))
+  (let ((modulus (mod a n)))
+    (pauli-x (+ 2 (* 2 ancilla)) qreg)
+    (loop for i from 1 to width do
+	 (setf modulus (mod a n))
+	 (loop for j from 1 to (1- i) do
+	      (setf modulus (* modulus modulus))
+	      (setf modulus (mod modulus n)))
+	 (multiply-mod n modulus (+ 1 i (* 3 ancilla)) ancilla qreg)
+       ;; And return?
+	 )))
 
-(defun %-+ (qreg1 qreg2)
-  "Binary helper function of add algorithm."
-  (declare (type quantum-register qreg1 qreg2))
+(defun *-mod (n mod control ancilla qreg)
   )
 
-(defun %-+-mod (qreg1 qreg2 mod)
-  "Binary helper function of add-mod algorithm."
-  (declare (type quantum-register qreg1 qreg2 mod))
+(defun +-mod (n mod control ancilla qreg)
   )
 
-(defun %-*-mod (qreg1 qreg2 mod)
-  "Binary helper function of mul-mod algorithm."
-  (declare (type quantum-register qreg1 qreg2 mod))
+(defun +-cdkm ()
   )
-
-(defun %-expt-mod (qreg1 qreg2 mod)
-  "Binary helper function of expt-mod algorithm."
-  (declare (type quantum-register qreg1 qreg2 mod))
-  )
-
